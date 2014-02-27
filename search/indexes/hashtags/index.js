f95fd67b9f832a5e2c7906a93e14aad48f531d7a
Steve Winton <stevewinton@gmail.com>
2014-02-27 14:31:39 -0600

    First commit

---
+++ search/indexes/hashtags/index.js
@@ -0,0 +1,27 @@
+function (doc) {
+    var created_at, year, month, day, hour, minute, timestamp;
+
+    /*
+    {
+        "_id": _id,
+        "stream": id,
+        "type": "hashtag",
+        "data": {
+            hashtag: hashtag,
+            created_at: created_at
+        }
+    }
+    */
+
+    if (doc.data.created_at) {
+        created_at = new Date(doc.data.created_at);
+        year = created_at.getUTCFullYear();
+        month = created_at.getUTCMonth() + 1;
+        day = created_at.getUTCDate();
+        hour = created_at.getUTCHours();
+        minute = created_at.getUTCMinutes();
+        timestamp = created_at.getTime();
+    }
+
+    index("stream", doc.stream);
+}
